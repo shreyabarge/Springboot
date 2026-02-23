455b06667b2b0777cc0a28e5b94eadaf095d97b3
Shreya Barge <shreyabarge1707@gmail.com>
2026-02-23 21:42:53 +0530

    Video from telusko YT channel

---
+++ 02_DependencyInjectionWithSpringBoot/src/main/java/com/myLearning/DependencyInjection/Dev.java
@@ -0,0 +1,10 @@
+package com.myLearning.DependencyInjection;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+      public void build() {
+    	  System.out.println("Working on awesome project..");
+      }
+}
